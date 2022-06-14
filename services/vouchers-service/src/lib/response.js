ec4ee65e598ff47305a4b864744c703f3fda1b7f
thejsman <niranjan.bhambi@gmail.com>
2022-06-14 10:42:13 +0530

    update voucher service for prod setup

--- services/vouchers-service/src/lib/response.js
+++ services/vouchers-service/src/lib/response.js
@@ -2,7 +2,7 @@ export const responseHandler = ({ statusCode, message, data }) => {
 	return {
 		statusCode,
 		headers: {
-			"Access-Control-Allow-Origin": "*.sagoon.com",
+			"Access-Control-Allow-Origin": process.env.ACCESS_CONTROL_ALLOW_ORIGIN,
 			"Access-Control-Allow-Credentials": true,
 			"Access-Control-Allow-Headers": "AccessToken",
 			preflightContinue: false,
