1af56df678e5c828df5f40fd05ff36615832cf1f
Ankit Rathi <ankit.rathi@sagoon.com>
2022-07-28 14:23:51 +0530

    finally send sms in invitation is working fine

--- services/invitation-service/src/handlers/create.js
+++ services/invitation-service/src/handlers/create.js
@@ -5,10 +5,13 @@ import { saveInvitation, radmonAlpha, sendSMS } from "../lib/invitationHelper";
 
 const createInvitationCode = async (event, context) => {
   try {
-    let { phone, dialCode, firstName, lastName, receiversList, bulk } =
-      JSON.parse(event.body);
+    let { phone, dialCode, firstName, lastName, receiversList } = JSON.parse(
+      event.body
+    );
+    let bulk = true;
     if (!receiversList) {
       receiversList = [{ phone, dialCode, firstName, lastName }];
+      bulk = false;
     }
     let result = await Promise.all(
       receiversList.map(async ({ phone, dialCode, firstName, lastName }) => {
@@ -31,8 +34,6 @@ const createInvitationCode = async (event, context) => {
       })
     );
 
-    console.log("resultObj", result);
-
     let response = responseHandler({
       statusCode: 200,
       message: "Invitation Code",
@@ -40,6 +41,7 @@ const createInvitationCode = async (event, context) => {
     });
     return response;
   } catch (error) {
+    console.log("error", error);
     return responseHandler({
       statusCode: 500,
       message: "Internal Server Error",
