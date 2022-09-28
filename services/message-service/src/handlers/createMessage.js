6038598b7d2a466e2ee43fa958f3d9f9fd9e855d
thejsman <niranjan.bhambi@gmail.com>
2022-09-28 15:17:36 +0530

    update message service

--- services/message-service/src/handlers/createMessage.js
+++ services/message-service/src/handlers/createMessage.js
@@ -6,31 +6,30 @@ import commonMiddleware from "../../../../packages/common-middleware";
 import moment from "moment";
 
 const sendMessage = async (event, context) => {
-	const now = new Date();
-	try {
-		const { countryCode, createdDate, smsText } =
-			event.body;
-
-		await saveMessage({
-			smsText: smsText,
-			countryCode: countryCode,
-			createdAt: now.toISOString(),
-			createdDate: moment().format("YYYY-MM-DD"),
-		});
-		await putSendMessageOnSQS({ countryCode, createdDate, smsText });
-		return responseHandler({
-			statusCode: 200,
-			message: "Message has been sent",
-			data: {},
-		});
-	} catch (error) {
-		console.log("error", error);
-		return responseHandler({
-			statusCode: 500,
-			message: "Internal Server Error",
-			data: {},
-		});
-	}
+  const now = new Date();
+  try {
+    const { dialCode, createdDate, smsText } = event.body;
+    console.log(dialCode, createdDate, smsText);
+    await saveMessage({
+      smsText,
+      dialCode,
+      createdAt: now.toISOString(),
+      createdDate: moment().format("YYYY-MM-DD"),
+    });
+    await putSendMessageOnSQS({ dialCode, createdDate, smsText });
+    return responseHandler({
+      statusCode: 200,
+      message: "Message has been sent",
+      data: {},
+    });
+  } catch (error) {
+    console.log("error", error);
+    return responseHandler({
+      statusCode: 500,
+      message: "Internal Server Error",
+      data: {},
+    });
+  }
 };
 
 // export const handler = userExists;
--- services/message-service/src/handlers/sendMessageQueue.js
+++ services/message-service/src/handlers/sendMessageQueue.js
@@ -1,24 +1,29 @@
-import { sendMessageViaAakash, sendIndiaSMS, getUsers } from "../lib/messageHelper";
+import {
+  sendMessageViaAakash,
+  //   sendIndiaSMS,
+  getUsers,
+} from "../lib/messageHelper";
 
 async function sendMessageQueue(event, context) {
-    try {
-        const record = event.Records[0];
-        const { smsText, countryCode, createdDate } = await JSON.parse(record.body);
-        let result = await getUsers({ countryCode, createdDate });
-        if (countryCode === "IN") {
-            for (let i = 0; i < result.length; i++) {
-                await sendIndiaSMS(result[i].pk, smsText);
-            }
-        }
-        else {
-            for (let i = 0; i < result.length; i++) {
-                await sendMessageViaAakash(result[i].phone, smsText);
-            }
-        }
-        console.log("numbers", result);
-    } catch (error) {
-        console.log("Exception in cleanPreUser", { error });
+  try {
+    const record = event.Records[0];
+    const { smsText, dialCode, createdDate } = await JSON.parse(record.body);
+    console.log({ smsText, dialCode, createdDate });
+    let result = await getUsers({ dialCode, createdDate });
+    if (dialCode === "91") {
+      //   for (let i = 0; i < result.length; i++) {
+      //     await sendIndiaSMS(result[i].pk, smsText);
+      //   }
+    } else {
+      for (let i = 0; i < result.length; i++) {
+        // Do nothing
+        await sendMessageViaAakash(result[i].phone, smsText);
+      }
     }
+    console.log("numbers", result);
+  } catch (error) {
+    console.log("Exception in cleanPreUser", { error });
+  }
 }
 
 export const handler = sendMessageQueue;
