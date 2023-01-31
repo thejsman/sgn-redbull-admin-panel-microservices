dd762aba41d7a3c9236bf5baadd4efea73a9f1e9
ankit <ankit.rathi@sagoon.com>
2023-01-31 17:31:38 +0530

    dau & mau now inserting

--- services/dau-mau-service/src/handlers/syncingDauMauInDb.js
+++ services/dau-mau-service/src/handlers/syncingDauMauInDb.js
@@ -1,18 +1,23 @@
 import { getRedisSet } from "redis-middleware";
-
+import { updateUserLoggedInActivityStats } from "../lib/utils";
 const syncingDauMauInDb = async (event, context) => {
   try {
     //get today date and also previous date and extract the data from redis and store in dynmo
     const now = new Date();
-    let yesterday = new Date(now.setDate(now.getDate() - 1));
     let currDate = now.toISOString().slice(0, 10);
-    let prevDate = yesterday.toISOString().slice(0, 10);
     let currMonth = now.toISOString().slice(0, 7);
+    let yesterday = new Date(now.setDate(now.getDate() - 1));
+    let prevDate = yesterday.toISOString().slice(0, 10);
     let prevMonth = yesterday.toISOString().slice(0, 7);
     console.log(currDate, prevDate, currMonth, prevMonth);
-    //logged this user id for today date in redis
-    let result = await getRedisSet("2023-01-03");
-    console.log('result', JSON.parse(JSON.parse(result.Payload)));
+    //Now extract the infofrom redis for these 4 parameters
+    let [currDateData, prevDateData, currMonthData, prevMonthData] = await Promise.all([getRedisSet(currDate), getRedisSet(prevDate), getRedisSet(currMonth), (currMonth != prevMonth) ? getRedisSet(prevMonth) : '']);
+    currDateData = currDateData.Payload ? JSON.parse(JSON.parse(currDateData.Payload)) : '';
+    prevDateData = prevDateData.Payload ? JSON.parse(JSON.parse(prevDateData.Payload)) : '';
+    currMonthData = currMonthData.Payload ? JSON.parse(JSON.parse(currMonthData.Payload)) : '';
+    prevMonthData = prevMonthData.Payload ? JSON.parse(JSON.parse(prevMonthData.Payload)) : '';
+    //first extract the data from database
+    await updateUserLoggedInActivityStats({ currDate, currMonth, prevDate, prevMonth, currDateCount: currDateData && currDateData.length > 0 ? currDateData.length : 0, currMonthCount: currMonthData && currMonthData.length > 0 ? currMonthData.length : 0, prevDateCount: prevDateData && prevDateData.length > 0 ? prevDateData.length : 0, prevMonthCount: prevMonthData && prevMonthData.length ? prevMonthData.length : 0 });
   } catch (error) {
     console.log("error", error);
   }
---
+++ services/dau-mau-service/src/lib/utils.js
@@ -0,0 +1,97 @@
+import AWS from "aws-sdk";
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+export const updateUserLoggedInActivityStats = async (payload) => {
+    //waitlistedUser registration date, fetching it from payload
+    let { currDate, currMonth, prevDate, prevMonth, currDateCount, currMonthCount, prevDateCount, prevMonthCount } = payload;
+    let prevMonthCountQuery = '';
+    let currMonthCountQuery = '';
+    currMonthCountQuery = prepareUserLoggedInActivityStatsFetchQuery(currMonth, "allCounts");
+    if (currMonth !== prevMonth) {
+        prevMonthCountQuery = prepareUserLoggedInActivityStatsFetchQuery(prevMonth, "allCounts");
+    }
+
+
+    try {
+        let currMonthData = dynamoDb.query(currMonthCountQuery).promise();
+        let prevMonthData = ((currMonth !== prevMonth) && prevMonthCount) ? dynamoDb.query(prevMonthCountQuery).promise() : '';
+        [currMonthData, prevMonthData] =
+            await Promise.all([currMonthData, prevMonthData]);
+        //extract the data object for current months
+        currMonthData = currMonthData.Items.length
+            ? currMonthData.Items[0]["countInfo"]
+            : {};
+        prevMonthData = prevMonthData && prevMonthData.Items.length ? prevMonthData.Items[0]["countInfo"]
+            : {};
+        //check if current date is available in data then update count by 1 if current date is not there then add data for curdate
+        currMonthData = currDateCount > 0 ? { ...currMonthData, [currDate]: { "date": currDate, "count": currDateCount } } : { ...currMonthData };
+        currMonthData = (currMonth == prevMonth) && prevDateCount > 0 ? { ...currMonthData, [prevDate]: { "date": prevDate, "count": prevDateCount } } : { ...currMonthData };
+        currMonthData = currMonthCount > 0 ? { ...currMonthData, "count": currMonthCount } : { ...currMonthData };
+        prevMonthData = (currMonth != prevMonth) && prevDateCount > 0 ? { ...prevMonthData, [prevDate]: { "date": prevDate, "count": prevDateCount } } : { ...prevMonthData };
+        prevMonthData = (currMonth != prevMonth) && prevMonthCount > 0 ? { ...prevMonthData, "count": prevMonthCount } : { ...prevMonthData };
+        //Now update the data into DynamoDB
+        console.log('currMonthData', currMonthData);
+        console.log('prevMonthData', prevMonthData);
+        //Now update the occasion data for perticular month & date
+        currMonthCountQuery = currDateCount > 0 || currMonthCount > 0 ? prepareUserActivityLoggedInStatsUpdateQuery(
+            currMonth,
+            "allCounts",
+            currMonthData
+        ) : '';
+        prevMonthCountQuery = prevMonthCount > 0 || ((prevMonth != currMonth) && prevDateCount > 0) ? prepareUserActivityLoggedInStatsUpdateQuery(
+            prevMonth,
+            "allCounts",
+            prevMonthData
+        ) : '';
+
+        let currMonthCountQueryStatus = currMonthCountQuery ? dynamoDb.update(currMonthCountQuery).promise() : '';
+        let prevMonthCountQueryStatus = prevMonthCountQuery ? dynamoDb.update(prevMonthCountQuery).promise() : '';
+        let updatedData = await Promise.all([
+            currMonthCountQueryStatus,
+            prevMonthCountQueryStatus
+        ]);
+
+        return updatedData;
+    } catch (error) {
+        console.log("Exception in user activity syncing DAU & MSAU", error);
+    }
+};
+
+const prepareUserLoggedInActivityStatsFetchQuery = (pk, sk) => {
+    return {
+        TableName: process.env.USERS_LOGGED_IN_ACTIVITY_STATS_TABLE,
+        KeyConditionExpression: "pk = :pk and #type=:type",
+        ExpressionAttributeNames: {
+            "#type": "type",
+        },
+        ExpressionAttributeValues: {
+            ":pk": `${pk}`,
+            ":type": `${sk}`,
+        },
+        ProjectionExpression: "countInfo",
+    };
+};
+
+const prepareUserActivityLoggedInStatsUpdateQuery = (pk, sk, data) => {
+    const updateExpression = generateUpdateQuery({ countInfo: data });
+    return {
+        TableName: process.env.USERS_LOGGED_IN_ACTIVITY_STATS_TABLE,
+        Key: { pk, type: sk },
+        ...updateExpression,
+        ReturnValues: "NONE",
+    };
+};
+
+export const generateUpdateQuery = (fields) => {
+    let exp = {
+        UpdateExpression: "set",
+        ExpressionAttributeNames: {},
+        ExpressionAttributeValues: {},
+    };
+    Object.entries(fields).forEach(([key, item]) => {
+        exp.UpdateExpression += ` #${key} = :${key},`;
+        exp.ExpressionAttributeNames[`#${key}`] = key;
+        exp.ExpressionAttributeValues[`:${key}`] = item;
+    });
+    exp.UpdateExpression = exp.UpdateExpression.replace(/(^,)|(,$)/g, "");
+    return exp;
+};
