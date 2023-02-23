ab384f5e8a0a489a39be613fe23a8e8ebef5e591
thejsman <niranjan.bhambi@gmail.com>
2023-02-23 16:22:44 +0530

    DAU service prod changes

--- services/dau-mau-service/src/handlers/runAthena.js
+++ services/dau-mau-service/src/handlers/runAthena.js
@@ -6,111 +6,240 @@ const runAthena = async (event, context) => {
   try {
     const params = {
       QueryExecutionContext: {
-        Database: process.env.ATHENA_DB_DMW
+        Database: process.env.ATHENA_DB_DMW,
       },
       QueryString: "",
       ResultConfiguration: {
-        OutputLocation: `s3://${process.env.ATHENA_BUCKET_FOR_DMW}/result`
-      }
+        OutputLocation: `s3://${process.env.ATHENA_BUCKET_FOR_DMW}/result`,
+      },
     };
     let now = new Date(new Date().getTime() + 19800000);
-    console.log('now.getHours()', process.env.TZ, new Date(), now.getHours(), now.getTime(), new Date().getTime(), now.toISOString());
+    console.log(
+      "now.getHours()",
+      process.env.TZ,
+      new Date(),
+      now.getHours(),
+      now.getTime(),
+      new Date().getTime(),
+      now.toISOString()
+    );
     let currDate, prevDate, currMonth, prevMonth, currWeek, prevWeek;
     currDate = prevDate = currMonth = prevMonth = currWeek = prevWeek = "";
-    let currDateQuery, prevDateQuery, currMonthQuery, prevMonthQuery, currWeekQuery, prevWeekQuery;
-    currDateQuery = prevDateQuery = currMonthQuery = prevMonthQuery = currWeekQuery = prevWeekQuery = "";
+    let currDateQuery,
+      prevDateQuery,
+      currMonthQuery,
+      prevMonthQuery,
+      currWeekQuery,
+      prevWeekQuery;
+    currDateQuery =
+      prevDateQuery =
+      currMonthQuery =
+      prevMonthQuery =
+      currWeekQuery =
+      prevWeekQuery =
+        "";
     //prepare query for currDate
     currDate = now.toISOString().slice(0, 10);
-    currDateQuery = { ...params, QueryString: `select count(*) from (select count(*) from loggedin_data where ${prepareQuery(currDate, 'daily')} group by uid)` };
+    currDateQuery = {
+      ...params,
+      QueryString: `select count(*) from (select count(*) from loggedin_data where ${prepareQuery(
+        currDate,
+        "daily"
+      )} group by uid)`,
+    };
     //check if hour is between 0,2 then run a query on prevDate also as we are assumingafter 12AM it will run for last day also once
     if (now.getHours() > 8 && now.getHours() < 10) {
       prevDate = new Date(now.setDate(now.getDate() - 1));
       prevDate = prevDate.toISOString().slice(0, 10);
-      prevDateQuery = { ...params, QueryString: `select count(*) from (select count(*) from loggedin_data where ${prepareQuery(prevDate, 'daily')} group by uid)` };
+      prevDateQuery = {
+        ...params,
+        QueryString: `select count(*) from (select count(*) from loggedin_data where ${prepareQuery(
+          prevDate,
+          "daily"
+        )} group by uid)`,
+      };
     }
     //for current month but we will run it only once in the night between 1, 2'o clock
     now = new Date(new Date().getTime() + 19800000);
     if (now.getHours() > 8 && now.getHours() < 10) {
       currMonth = now.toISOString().slice(0, 7);
-      currMonthQuery = { ...params, QueryString: `select count(*) from (select count(*) from loggedin_data where ${prepareQuery(currMonth, 'monthly')} group by uid)` };
+      currMonthQuery = {
+        ...params,
+        QueryString: `select count(*) from (select count(*) from loggedin_data where ${prepareQuery(
+          currMonth,
+          "monthly"
+        )} group by uid)`,
+      };
       //check if prev month is diffrent from current month
       prevMonth = new Date(now.setDate(now.getDate() - 1));
       prevMonth = prevMonth.toISOString().slice(0, 7);
       if (currMonth != prevMonth) {
-        prevMonthQuery = { ...params, QueryString: `select count(*) from (select count(*) from loggedin_data where ${prepareQuery(prevMonth, 'monthly')}  group by uid)` };
+        prevMonthQuery = {
+          ...params,
+          QueryString: `select count(*) from (select count(*) from loggedin_data where ${prepareQuery(
+            prevMonth,
+            "monthly"
+          )}  group by uid)`,
+        };
       }
       //now same for week first update for current week
       now = new Date(new Date().getTime() + 19800000);
       currWeek = weekNumber(now);
-      console.log('new Date(now.setDate(now.getDate() - 1)).toISOString()', new Date(now.setDate(now.getDate() - 1)).toISOString());
-      currWeekQuery =
-        currWeekQuery = datesOfTheWeek(currWeek.slice(0, 10)).map(cDate => {
-          return `year=${cDate.slice(0, 4)} and month=${cDate.slice(5, 7)} and day=${cDate.slice(8, 10)}`;
-        }).join(' or ');
-      currWeekQuery = { ...params, QueryString: `select count(*) from (select count(*) from loggedin_data where ${prepareWeeklyQuery(currWeek, currWeekQuery)} group by uid)` };
+      console.log(
+        "new Date(now.setDate(now.getDate() - 1)).toISOString()",
+        new Date(now.setDate(now.getDate() - 1)).toISOString()
+      );
+      currWeekQuery = currWeekQuery = datesOfTheWeek(currWeek.slice(0, 10))
+        .map((cDate) => {
+          return `year=${cDate.slice(0, 4)} and month=${cDate.slice(
+            5,
+            7
+          )} and day=${cDate.slice(8, 10)}`;
+        })
+        .join(" or ");
+      currWeekQuery = {
+        ...params,
+        QueryString: `select count(*) from (select count(*) from loggedin_data where ${prepareWeeklyQuery(
+          currWeek,
+          currWeekQuery
+        )} group by uid)`,
+      };
       //check previous week
-      prevWeek = weekNumber(new Date(now.setDate(now.getDate() - 1)).toISOString().slice(0, 10));
+      prevWeek = weekNumber(
+        new Date(now.setDate(now.getDate() - 1)).toISOString().slice(0, 10)
+      );
       if (currWeek != prevWeek) {
-        prevWeekQuery =
-          prevWeekQuery = datesOfTheWeek(prevWeek.slice(0, 10)).map(cDate => {
-            return `year=${cDate.slice(0, 4)} and month=${cDate.slice(5, 7)} and day=${cDate.slice(8, 10)}`;
-          }).join(' or ');
-        prevWeekQuery = { ...params, QueryString: `select count(*) from (select count(*) from loggedin_data where ${prepareWeeklyQuery(prevWeek, prevWeekQuery)} group by uid)` };
-
+        prevWeekQuery = prevWeekQuery = datesOfTheWeek(prevWeek.slice(0, 10))
+          .map((cDate) => {
+            return `year=${cDate.slice(0, 4)} and month=${cDate.slice(
+              5,
+              7
+            )} and day=${cDate.slice(8, 10)}`;
+          })
+          .join(" or ");
+        prevWeekQuery = {
+          ...params,
+          QueryString: `select count(*) from (select count(*) from loggedin_data where ${prepareWeeklyQuery(
+            prevWeek,
+            prevWeekQuery
+          )} group by uid)`,
+        };
       }
     }
-    console.log("currDateQuery", currDateQuery, "prevDateQuery", prevDateQuery, currMonthQuery, prevMonthQuery, currWeekQuery);
+    console.log(
+      "currDateQuery",
+      currDateQuery,
+      "prevDateQuery",
+      prevDateQuery,
+      currMonthQuery,
+      prevMonthQuery,
+      currWeekQuery
+    );
 
     //Now execute the query to athena
-    let [currDateQR, prevDateQR, currMonthQR, prevMonthQR, currWeekQR, prevWeekQR] = await Promise.all([
+    let [
+      currDateQR,
+      prevDateQR,
+      currMonthQR,
+      prevMonthQR,
+      currWeekQR,
+      prevWeekQR,
+    ] = await Promise.all([
       currDateQuery ? athena.startQueryExecution(currDateQuery).promise() : "",
       prevDateQuery ? athena.startQueryExecution(prevDateQuery).promise() : "",
-      currMonthQuery ? athena.startQueryExecution(currMonthQuery).promise() : "",
-      prevMonthQuery ? athena.startQueryExecution(prevMonthQuery).promise() : "",
+      currMonthQuery
+        ? athena.startQueryExecution(currMonthQuery).promise()
+        : "",
+      prevMonthQuery
+        ? athena.startQueryExecution(prevMonthQuery).promise()
+        : "",
       currWeekQuery ? athena.startQueryExecution(currWeekQuery).promise() : "",
       prevWeekQuery ? athena.startQueryExecution(prevWeekQuery).promise() : "",
     ]);
 
-    console.log('athenaResult---,currDateQR', currDateQR);
+    console.log("athenaResult---,currDateQR", currDateQR);
 
-    console.log('athenaResult---,prevDateQR', prevDateQR);
-    console.log('athenaResult---,currMonthQR', currMonthQR);
-    console.log('athenaResult---,prevMonthQR', prevMonthQR);
-    console.log('athenaResult---,currWeekQR', currWeekQR);
-    console.log('athenaResult---,prevWeekQR', prevWeekQR);
+    console.log("athenaResult---,prevDateQR", prevDateQR);
+    console.log("athenaResult---,currMonthQR", currMonthQR);
+    console.log("athenaResult---,prevMonthQR", prevMonthQR);
+    console.log("athenaResult---,currWeekQR", currWeekQR);
+    console.log("athenaResult---,prevWeekQR", prevWeekQR);
 
     //Now put all Query execution ids in cache for later fecth the result
-    await Promise.all([currDateQR?.QueryExecutionId ? setRedisItem(`DAU-${currDate}`, 7200, currDateQR?.QueryExecutionId) : "",
-    prevDateQR?.QueryExecutionId ? setRedisItem(`DAU-${prevDate}`, 7200, prevDateQR?.QueryExecutionId) : "",
-    currMonthQR?.QueryExecutionId ? setRedisItem(`MAU-${currMonth}`, 7200, currMonthQR?.QueryExecutionId) : "",
-    prevMonthQR?.QueryExecutionId ? setRedisItem(`MAU-${prevMonth}`, 7200, prevMonthQR?.QueryExecutionId) : "",
-    currWeekQR?.QueryExecutionId ? setRedisItem(`WAU-${currWeek}`, 7200, currWeekQR?.QueryExecutionId) : "",
-    prevWeekQR?.QueryExecutionId ? setRedisItem(`WAU-${prevWeek}`, 7200, prevWeekQR?.QueryExecutionId) : ""]);
-
+    await Promise.all([
+      currDateQR?.QueryExecutionId
+        ? setRedisItem(`DAU-${currDate}`, 7200, currDateQR?.QueryExecutionId)
+        : "",
+      prevDateQR?.QueryExecutionId
+        ? setRedisItem(`DAU-${prevDate}`, 7200, prevDateQR?.QueryExecutionId)
+        : "",
+      currMonthQR?.QueryExecutionId
+        ? setRedisItem(`MAU-${currMonth}`, 7200, currMonthQR?.QueryExecutionId)
+        : "",
+      prevMonthQR?.QueryExecutionId
+        ? setRedisItem(`MAU-${prevMonth}`, 7200, prevMonthQR?.QueryExecutionId)
+        : "",
+      currWeekQR?.QueryExecutionId
+        ? setRedisItem(`WAU-${currWeek}`, 7200, currWeekQR?.QueryExecutionId)
+        : "",
+      prevWeekQR?.QueryExecutionId
+        ? setRedisItem(`WAU-${prevWeek}`, 7200, prevWeekQR?.QueryExecutionId)
+        : "",
+    ]);
   } catch (error) {
     console.log("error", error);
-
   }
 };
 
 const prepareQuery = (date, term) => {
-  let query = '';
-  let lastDate = new Date(new Date(date).setDate(new Date(date).getDate() - 1)).toISOString();
-  let nextDate = new Date(new Date(date).setDate(new Date(date).getDate() + 1)).toISOString();
+  let query = "";
+  let lastDate = new Date(
+    new Date(date).setDate(new Date(date).getDate() - 1)
+  ).toISOString();
+  let nextDate = new Date(
+    new Date(date).setDate(new Date(date).getDate() + 1)
+  ).toISOString();
   let tsFrom = new Date(date).getTime() - 19800000;
   let tsTo = new Date(nextDate).getTime() - 19800000;
-  console.log('=> tsFrom new Date(date).getTime()', date, new Date(date).getTime());
+  console.log(
+    "=> tsFrom new Date(date).getTime()",
+    date,
+    new Date(date).getTime()
+  );
   switch (term) {
     case "daily":
-      query = `(year=${date.slice(0, 4)} and month=${date.slice(5, 7)} and day=${date.slice(8, 10)} or year=${lastDate.slice(0, 4)} and month=${lastDate.slice(5, 7)} and day=${lastDate.slice(8, 10)}) and ts >='${tsFrom}' and ts < '${tsTo}'`;
+      query = `(year=${date.slice(0, 4)} and month=${date.slice(
+        5,
+        7
+      )} and day=${date.slice(8, 10)} or year=${lastDate.slice(
+        0,
+        4
+      )} and month=${lastDate.slice(5, 7)} and day=${lastDate.slice(
+        8,
+        10
+      )}) and ts >='${tsFrom}' and ts < '${tsTo}'`;
       break;
     case "monthly":
-      tsFrom = new Date(`${date.slice(0, 4)}-${date.slice(5, 7)}-01`).getTime() - 19800000;
+      tsFrom =
+        new Date(`${date.slice(0, 4)}-${date.slice(5, 7)}-01`).getTime() -
+        19800000;
       let tsFromMonth = new Date(tsFrom).toISOString();
-      tsTo = new Date(`${date.slice(0, 4)}-${date.slice(5, 7)}-01`).setMonth(new Date(`${date.slice(0, 4)}-${date.slice(5, 7)}-01`).getMonth() + 1) - 19800000;
+      tsTo =
+        new Date(`${date.slice(0, 4)}-${date.slice(5, 7)}-01`).setMonth(
+          new Date(`${date.slice(0, 4)}-${date.slice(5, 7)}-01`).getMonth() + 1
+        ) - 19800000;
 
-      console.log('tsfrom', tsFrom, tsTo);
-      query = `(year=${date.slice(0, 4)} and month=${date.slice(5, 7)} or year=${tsFromMonth.slice(0, 4)} and month=${tsFromMonth.slice(5, 7)} and day=${tsFromMonth.slice(8, 10)}) and ts > '${tsFrom}' and ts<'${tsTo}'`;
+      console.log("tsfrom", tsFrom, tsTo);
+      query = `(year=${date.slice(0, 4)} and month=${date.slice(
+        5,
+        7
+      )} or year=${tsFromMonth.slice(0, 4)} and month=${tsFromMonth.slice(
+        5,
+        7
+      )} and day=${tsFromMonth.slice(
+        8,
+        10
+      )}) and ts > '${tsFrom}' and ts<'${tsTo}'`;
       break;
     default:
       break;
@@ -119,12 +248,25 @@ const prepareQuery = (date, term) => {
 };
 
 const prepareWeeklyQuery = (weekNum, query) => {
-  let lastDate = new Date(new Date(weekNum.slice(0, 10)).setDate(new Date(weekNum.slice(0, 10)).getDate() - 1)).toISOString();
-  let nextDate = new Date(new Date(weekNum.slice(-10)).setDate(new Date(weekNum.slice(-10)).getDate() + 1)).toISOString();
+  let lastDate = new Date(
+    new Date(weekNum.slice(0, 10)).setDate(
+      new Date(weekNum.slice(0, 10)).getDate() - 1
+    )
+  ).toISOString();
+  let nextDate = new Date(
+    new Date(weekNum.slice(-10)).setDate(
+      new Date(weekNum.slice(-10)).getDate() + 1
+    )
+  ).toISOString();
   let tsFrom = new Date(weekNum.slice(0, 10)).getTime() - 19800000;
   let tsTo = new Date(nextDate).getTime() - 19800000;
-  return `(year=${lastDate.slice(0, 4)} and month=${lastDate.slice(5, 7)} and day=${lastDate.slice(8, 10)} or ${query}) and ts>'${tsFrom}' and ts<'${tsTo}'`;
-
+  return `(year=${lastDate.slice(0, 4)} and month=${lastDate.slice(
+    5,
+    7
+  )} and day=${lastDate.slice(
+    8,
+    10
+  )} or ${query}) and ts>'${tsFrom}' and ts<'${tsTo}'`;
 };
 
 export const handler = runAthena;
--- services/dau-mau-service/src/handlers/syncingDauMauInDb.js
+++ services/dau-mau-service/src/handlers/syncingDauMauInDb.js
@@ -7,9 +7,9 @@ const athena = new AWS.Athena();
 const syncingDauMauInDb = async (event, context) => {
   try {
     //get today date and also previous date and extract the data from redis and store in dynmo
-    const redisPrefixForDau = 'DAU';
-    const redisPrefixForMau = 'MAU';
-    const redisPrefixForWau = 'WAU';
+    const redisPrefixForDau = "DAU";
+    const redisPrefixForMau = "MAU";
+    const redisPrefixForWau = "WAU";
     const now = new Date(new Date().getTime() + 19800000);
     let currDate = now.toISOString().slice(0, 10);
     let currMonth = now.toISOString().slice(0, 7);
@@ -19,53 +19,204 @@ const syncingDauMauInDb = async (event, context) => {
     let currWeek = weekNumber(currDate);
     let prevWeek = weekNumber(yesterday);
     let rcks = []; //cache keys to be removed
-    console.log('now', now.toISOString(), yesterday.toISOString());
+    console.log("now", now.toISOString(), yesterday.toISOString());
 
     //Now extract the infofrom redis for these 4 parameters
-    let [currDateQID, prevDateQID, currMonthQID, prevMonthQID, currWeekQID, prevWeekQID] = await Promise.all([getRedisItem(`${redisPrefixForDau}-${currDate}`), getRedisItem(`${redisPrefixForDau}-${prevDate}`), getRedisItem(`${redisPrefixForMau}-${currMonth}`), getRedisItem(`${redisPrefixForMau}-${prevMonth}`), getRedisItem(`${redisPrefixForWau}-${currWeek}`), getRedisItem(`${redisPrefixForWau}-${prevWeek}`)]);
-    console.log('prevDateQID', currDateQID ? 3 : 4, prevDateQID ? 1 : 2, currMonthQID, prevMonthQID, currWeekQID, prevWeekQID);
-    currDateQID = currDateQID.Payload ? JSON.parse(JSON.parse(currDateQID.Payload)) : '';
-    prevDateQID = prevDateQID.Payload ? JSON.parse(JSON.parse(prevDateQID.Payload)) : '';
-    currMonthQID = currMonthQID.Payload ? JSON.parse(JSON.parse(currMonthQID.Payload)) : '';
-    prevMonthQID = prevMonthQID.Payload ? JSON.parse(JSON.parse(prevMonthQID.Payload)) : '';
-    currWeekQID = currWeekQID.Payload ? JSON.parse(JSON.parse(currWeekQID.Payload)) : '';
-    prevWeekQID = prevWeekQID.Payload ? JSON.parse(JSON.parse(prevWeekQID.Payload)) : '';
-    console.log('prevDateQID', currDateQID ? 3 : 4, prevDateQID ? 1 : 2, currMonthQID, prevMonthQID, currWeekQID, prevWeekQID);
-    let [currDateAR, prevDateAR, currMonthAR, prevMonthAR, currWeekAR, prevWeekAR] = await Promise.all([
-      currDateQID ? athena.getQueryResults({ QueryExecutionId: currDateQID }).promise().catch(e => { rcks = e.message.includes('FAILED') ? [...rcks, `${redisPrefixForDau}-${currDate}`] : [...rcks]; }) : "",
-      prevDateQID ? athena.getQueryResults({ QueryExecutionId: prevDateQID }).promise().catch(e => { rcks = e.message.includes('FAILED') ? [...rcks, `${redisPrefixForDau}-${prevDate}`] : [...rcks]; }) : "",
-      currMonthQID ? athena.getQueryResults({ QueryExecutionId: currMonthQID }).promise().catch(e => { rcks = e.message.includes('FAILED') ? [...rcks, `${redisPrefixForMau}-${currMonth}`] : [...rcks]; }) : "",
-      prevMonthQID && currMonth != prevMonth ? athena.getQueryResults({ QueryExecutionId: prevMonthQID }).promise().catch(e => { rcks = e.message.includes('FAILED') ? [...rcks, `${redisPrefixForMau}-${prevMonth}`] : [...rcks]; }) : "",
-      currWeekQID ? athena.getQueryResults({ QueryExecutionId: currWeekQID }).promise().catch(e => { rcks = e.message.includes('FAILED') ? [...rcks, `${redisPrefixForWau}-${currWeek}`] : [...rcks]; }) : "",
-      prevWeekQID && currWeek != prevWeek ? athena.getQueryResults({ QueryExecutionId: prevWeekQID }).promise().catch(e => { rcks = e.message.includes('FAILED') ? [...rcks, `${redisPrefixForWau}-${prevWeek}`] : [...rcks]; }) : "",
+    let [
+      currDateQID,
+      prevDateQID,
+      currMonthQID,
+      prevMonthQID,
+      currWeekQID,
+      prevWeekQID,
+    ] = await Promise.all([
+      getRedisItem(`${redisPrefixForDau}-${currDate}`),
+      getRedisItem(`${redisPrefixForDau}-${prevDate}`),
+      getRedisItem(`${redisPrefixForMau}-${currMonth}`),
+      getRedisItem(`${redisPrefixForMau}-${prevMonth}`),
+      getRedisItem(`${redisPrefixForWau}-${currWeek}`),
+      getRedisItem(`${redisPrefixForWau}-${prevWeek}`),
     ]);
+    console.log("currDateQID-----", currDateQID);
+    console.log(
+      "prevDateQID",
+      currDateQID ? 3 : 4,
+      prevDateQID ? 1 : 2,
+      currMonthQID,
+      prevMonthQID,
+      currWeekQID,
+      prevWeekQID
+    );
+    currDateQID = currDateQID.Payload
+      ? JSON.parse(JSON.parse(currDateQID.Payload))
+      : "";
+    prevDateQID = prevDateQID.Payload
+      ? JSON.parse(JSON.parse(prevDateQID.Payload))
+      : "";
+    currMonthQID = currMonthQID.Payload
+      ? JSON.parse(JSON.parse(currMonthQID.Payload))
+      : "";
+    prevMonthQID = prevMonthQID.Payload
+      ? JSON.parse(JSON.parse(prevMonthQID.Payload))
+      : "";
+    currWeekQID = currWeekQID.Payload
+      ? JSON.parse(JSON.parse(currWeekQID.Payload))
+      : "";
+    prevWeekQID = prevWeekQID.Payload
+      ? JSON.parse(JSON.parse(prevWeekQID.Payload))
+      : "";
+    console.log(
+      "prevDateQID",
+      currDateQID ? 3 : 4,
+      prevDateQID ? 1 : 2,
+      currMonthQID,
+      prevMonthQID,
+      currWeekQID,
+      prevWeekQID
+    );
+    console.log("{ QueryExecutionId: currDateQID }", {
+      QueryExecutionId: currDateQID,
+    });
+    let [
+      currDateAR,
+      prevDateAR,
+      currMonthAR,
+      prevMonthAR,
+      currWeekAR,
+      prevWeekAR,
+    ] = await Promise.all([
+      currDateQID
+        ? athena
+            .getQueryResults({ QueryExecutionId: currDateQID })
+            .promise()
+            .catch((e) => {
+              rcks = e.message.includes("FAILED")
+                ? [...rcks, `${redisPrefixForDau}-${currDate}`]
+                : [...rcks];
+            })
+        : "",
+      prevDateQID
+        ? athena
+            .getQueryResults({ QueryExecutionId: prevDateQID })
+            .promise()
+            .catch((e) => {
+              rcks = e.message.includes("FAILED")
+                ? [...rcks, `${redisPrefixForDau}-${prevDate}`]
+                : [...rcks];
+            })
+        : "",
+      currMonthQID
+        ? athena
+            .getQueryResults({ QueryExecutionId: currMonthQID })
+            .promise()
+            .catch((e) => {
+              rcks = e.message.includes("FAILED")
+                ? [...rcks, `${redisPrefixForMau}-${currMonth}`]
+                : [...rcks];
+            })
+        : "",
+      prevMonthQID && currMonth != prevMonth
+        ? athena
+            .getQueryResults({ QueryExecutionId: prevMonthQID })
+            .promise()
+            .catch((e) => {
+              rcks = e.message.includes("FAILED")
+                ? [...rcks, `${redisPrefixForMau}-${prevMonth}`]
+                : [...rcks];
+            })
+        : "",
+      currWeekQID
+        ? athena
+            .getQueryResults({ QueryExecutionId: currWeekQID })
+            .promise()
+            .catch((e) => {
+              rcks = e.message.includes("FAILED")
+                ? [...rcks, `${redisPrefixForWau}-${currWeek}`]
+                : [...rcks];
+            })
+        : "",
+      prevWeekQID && currWeek != prevWeek
+        ? athena
+            .getQueryResults({ QueryExecutionId: prevWeekQID })
+            .promise()
+            .catch((e) => {
+              rcks = e.message.includes("FAILED")
+                ? [...rcks, `${redisPrefixForWau}-${prevWeek}`]
+                : [...rcks];
+            })
+        : "",
+    ]);
+    console.log("currDateAR==>", JSON.stringify(currDateAR));
     currDateAR = currDateAR?.ResultSet?.Rows?.[1]?.Data?.[0]?.VarCharValue ?? 0;
     prevDateAR = prevDateAR?.ResultSet?.Rows?.[1]?.Data?.[0]?.VarCharValue ?? 0;
-    currMonthAR = currMonthAR?.ResultSet?.Rows?.[1]?.Data?.[0]?.VarCharValue ?? 0;
-    prevMonthAR = prevMonthAR?.ResultSet?.Rows?.[1]?.Data?.[0]?.VarCharValue ?? 0;
+    currMonthAR =
+      currMonthAR?.ResultSet?.Rows?.[1]?.Data?.[0]?.VarCharValue ?? 0;
+    prevMonthAR =
+      prevMonthAR?.ResultSet?.Rows?.[1]?.Data?.[0]?.VarCharValue ?? 0;
     currWeekAR = currWeekAR?.ResultSet?.Rows?.[1]?.Data?.[0]?.VarCharValue ?? 0;
     prevWeekAR = prevWeekAR?.ResultSet?.Rows?.[1]?.Data?.[0]?.VarCharValue ?? 0;
-    console.log('currDateAR', currDateAR);
-    console.log('prevDateAR', prevDateAR);
-    console.log('currMonthQID', currMonthAR);
-    console.log('prevMonthQID', prevMonthAR);
-    console.log('currWeekQID', currWeekAR);
-    console.log('prevWeekQID', prevWeekAR);
+    console.log("currDateAR", currDateAR);
+    console.log("prevDateAR", prevDateAR);
+    console.log("currMonthQID", currMonthAR);
+    console.log("prevMonthQID", prevMonthAR);
+    console.log("currWeekQID", currWeekAR);
+    console.log("prevWeekQID", prevWeekAR);
     //remove failed and suceed calls from cache also
-    rcks = currDateAR ? [...rcks, `${redisPrefixForDau}-${currDate}`] : [...rcks];
-    rcks = prevDateAR ? [...rcks, `${redisPrefixForDau}-${prevDate}`] : [...rcks];
-    rcks = currMonthAR ? [...rcks, `${redisPrefixForMau}-${currMonth}`] : [...rcks];
-    rcks = prevMonthAR ? [...rcks, `${redisPrefixForMau}-${prevMonth}`] : [...rcks];
-    rcks = currWeekAR ? [...rcks, `${redisPrefixForWau}-${currWeek}`] : [...rcks];
-    rcks = prevWeekAR ? [...rcks, `${redisPrefixForWau}-${prevWeek}`] : [...rcks];
-    console.log('redis keys to be removed', rcks);
+    rcks = currDateAR
+      ? [...rcks, `${redisPrefixForDau}-${currDate}`]
+      : [...rcks];
+    rcks = prevDateAR
+      ? [...rcks, `${redisPrefixForDau}-${prevDate}`]
+      : [...rcks];
+    rcks = currMonthAR
+      ? [...rcks, `${redisPrefixForMau}-${currMonth}`]
+      : [...rcks];
+    rcks = prevMonthAR
+      ? [...rcks, `${redisPrefixForMau}-${prevMonth}`]
+      : [...rcks];
+    rcks = currWeekAR
+      ? [...rcks, `${redisPrefixForWau}-${currWeek}`]
+      : [...rcks];
+    rcks = prevWeekAR
+      ? [...rcks, `${redisPrefixForWau}-${prevWeek}`]
+      : [...rcks];
+    console.log("redis keys to be removed", rcks);
     //first extract the data from database
-    await updateUserLoggedInActivityStats({ currDate, currMonth, prevDate, prevMonth, currDateCount: +currDateAR, currMonthCount: +currMonthAR, prevDateCount: +prevDateAR, prevMonthCount: +prevMonthAR, currWeek, prevWeek, currWeekCount: +currWeekAR, prevWeekCount: +prevWeekAR });
+    await updateUserLoggedInActivityStats({
+      currDate,
+      currMonth,
+      prevDate,
+      prevMonth,
+      currDateCount: +currDateAR,
+      currMonthCount: +currMonthAR,
+      prevDateCount: +prevDateAR,
+      prevMonthCount: +prevMonthAR,
+      currWeek,
+      prevWeek,
+      currWeekCount: +currWeekAR,
+      prevWeekCount: +prevWeekAR,
+    });
+    console.log("data to be stored--", {
+      currDate,
+      currMonth,
+      prevDate,
+      prevMonth,
+      currDateCount: +currDateAR,
+      currMonthCount: +currMonthAR,
+      prevDateCount: +prevDateAR,
+      prevMonthCount: +prevMonthAR,
+      currWeek,
+      prevWeek,
+      currWeekCount: +currWeekAR,
+      prevWeekCount: +prevWeekAR,
+    });
     //Now set redis item ttl zero so that it will be deleted from redis
-    await Promise.all(rcks.map(key => {
-      console.log('key', key);
-      return setRedisItem(key, 10, 'ToBeDeleted');
-    }));
+    await Promise.all(
+      rcks.map((key) => {
+        console.log("key", key);
+        return setRedisItem(key, 10, "ToBeDeleted");
+      })
+    );
   } catch (error) {
     console.log("error", error);
   }
