4937486a98a879aa5a4db4288278204d918e107f
dev vats <vatsdev27@gmail.com>
2023-05-09 11:42:28 +0530

    convert to array from string in occasion name

--- services/occasion-message-service/src/handlers/AddMessageInOccasion.js
+++ services/occasion-message-service/src/handlers/AddMessageInOccasion.js
@@ -4,15 +4,25 @@ import { putMessageInOccasion } from "../lib/occasionMessageHelper";
 async function AddMessageInOccasion(event, context) {
   let now = new Date();
   let { displayOrder, occasionName, message, isHost } = event.body;
-  let messageParams = {
-    displayOrder,
-    occasionName,
-    createdAt: now.toISOString(),
-    message,
-    isHost,
-  };
+  // let messageParams = {
+  //   displayOrder,
+  //   occasionName,
+  //   createdAt: now.toISOString(),
+  //   message,
+  //   isHost,
+  // };
   try {
-    await putMessageInOccasion(messageParams);
+    const promises = occasionName.map((item) => {
+      return putMessageInOccasion({
+        displayOrder,
+        occasionName: item,
+        createdAt: now.toISOString(),
+        message,
+        isHost,
+      });
+    });
+    await Promise.all(promises);
+    //  await putMessageInOccasion(messageParams);
     return responseHandler({
       statusCode: 200,
       message: "Message Saved",
