c6de822790af498905cefcf873354029a7e0d02f
thejsman <niranjan.bhambi@gmail.com>
2023-06-29 11:00:20 +0530

    update CORS

--- services/occasion-service/src/lib/utils.js
+++ services/occasion-service/src/lib/utils.js
@@ -2,11 +2,9 @@ export function response(statusCode, data) {
   return {
     statusCode,
     headers: {
-      "Access-Control-Allow-Origin": process.env.ACCESS_CONTROL_ALLOW_ORIGIN,
-      "Access-Control-Allow-Credentials": true,
-      "Access-Control-Allow-Methods": "POST,PATCH",
-      "Access-Control-Allow-Headers": "Origin, Content-Type, Accept",
-      preflightContinue: false,
+      "access-control-allow-origin": "*",
+      "Access-Control-Allow-Headers": "*",
+      "Access-Control-Allow-Methods": "*",
     },
     body: JSON.stringify(data),
   };
